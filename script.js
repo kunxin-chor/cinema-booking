f8c6ecca50db33c6e7ec24c237051f7ffab85783
kunxin-chor <chorkunxin@yahoo.com>
2025-03-26 07:14:21 +0000

    Modified starter code

---
+++ script.js
@@ -0,0 +1,7 @@
+const movies = {
+    movie1: "Interstellar: Director's Cut",
+    movie2: "The French Dispatch",
+    movie3: "Dune: Part Two",
+    movie4: "The Batman",
+    movie5: "Everything Everywhere All at Once"
+};
